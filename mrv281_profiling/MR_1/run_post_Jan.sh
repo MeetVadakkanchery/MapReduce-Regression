ca57c2498569981c0e278a445975ccf9a0d5983a
MeetVadakkanchery <47645660+MeetVadakkanchery@users.noreply.github.com>
2021-05-16 14:02:14 -0400

    Add files via upload

---
+++ mrv281_profiling/MR_1/run_post_Jan.sh
@@ -0,0 +1,9 @@
+rm -rf output
+javac -classpath `yarn classpath` -d . src/CountRecsMapper.java
+javac -classpath `yarn classpath` -d . src/CountRecsReducer.java
+javac -classpath `yarn classpath`:. -d . src/CountRecs.java
+jar -cvf countRecs.jar *.class
+hadoop jar countRecs.jar CountRecs /user/mrv281/Project_Data/Cleaned_Data_Jan.txt /user/mrv281/output
+hadoop fs -get output
+hdfs dfs -rm -r output
+cat output/part-r-00000
