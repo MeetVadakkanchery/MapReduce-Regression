013411c45a15e259937f43fe419119b8bfe6fcdd
MeetVadakkanchery <47645660+MeetVadakkanchery@users.noreply.github.com>
2021-05-16 13:57:54 -0400

    Add files via upload

---
+++ mrv281_profiling/Demand_Data/run_Apr.sh
@@ -0,0 +1,11 @@
+rm -rf output
+rm -rf output
+javac -classpath `yarn classpath` -d . src/DemandMapper.java
+javac -classpath `yarn classpath` -d . src/DemandReducer.java
+javac -classpath `yarn classpath`:. -d . src/Demand.java
+jar -cvf demandData.jar *.class
+hadoop jar demandData.jar Demand /user/mrv281/Project_Data/Cleaned_Data_Apr.txt /user/mrv281/output
+hadoop fs -get output
+hdfs dfs -rm -r output
+cat output/part-r-00000
+mv output/part-r-00000 output/Demand_Data_Apr.txt
