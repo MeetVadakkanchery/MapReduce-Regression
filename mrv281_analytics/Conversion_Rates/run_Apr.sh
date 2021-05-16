bf0718f2c4cf25f93a475deda889a07c534faf4e
MeetVadakkanchery <47645660+MeetVadakkanchery@users.noreply.github.com>
2021-05-16 13:56:37 -0400

    Add files via upload

---
+++ mrv281_analytics/Conversion_Rates/run_Apr.sh
@@ -0,0 +1,10 @@
+rm -rf output
+javac -classpath `yarn classpath` -d . src/ConversionMapper.java
+javac -classpath `yarn classpath` -d . src/ConversionReducer.java
+javac -classpath `yarn classpath`:. -d . src/Conversion.java
+jar -cvf conversionData.jar *.class
+hadoop jar conversionData.jar Conversion /user/mrv281/Project_Data/Event_Data_Apr.txt /user/mrv281/output
+hadoop fs -get output
+hdfs dfs -rm -r output
+cat output/part-r-00000
+mv output/part-r-00000 output/Conversion_Data_Apr.txt
