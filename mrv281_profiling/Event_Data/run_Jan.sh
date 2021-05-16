dd5e04854cc4cac3f9e9c44d9d5f6309f19ed67a
MeetVadakkanchery <47645660+MeetVadakkanchery@users.noreply.github.com>
2021-05-16 13:59:06 -0400

    Add files via upload

---
+++ mrv281_profiling/Event_Data/run_Jan.sh
@@ -0,0 +1,10 @@
+rm -rf output
+javac -classpath `yarn classpath` -d . src/EventMapper.java
+javac -classpath `yarn classpath` -d . src/EventReducer.java
+javac -classpath `yarn classpath`:. -d . src/Event.java
+jar -cvf eventData.jar *.class
+hadoop jar eventData.jar Event /user/mrv281/Project_Data/Cleaned_Data_Jan.txt /user/mrv281/output
+hadoop fs -get output
+hdfs dfs -rm -r output
+cat output/part-r-00000
+mv output/part-r-00000 output/Event_Data_Jan.txt
